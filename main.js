cc998c20c40956f93120af59271ef46ecd23f0a9
Natalia Kalinina <natalia.kalinina2907@gmail.com>
2020-03-14 21:51:27 +0200

    done

---
+++ main.js
@@ -0,0 +1,136 @@
+console.log('Sample JavaScript HW#1');
+
+/*
+ * #1
+ *
+ * Создайте переменные со значениями
+ */
+
+// имя переменной: myNum, значение: 10
+var myNum = 10;
+// имя переменной: myStr, значение: 'строка'
+var myStr = 'строка';
+// имя переменной: myBool, значение: true
+var myBool = true;
+// имя переменной: myArr, значения: 1, 2, 3, 4, 5
+var myArr = [1, 2, 3, 4, 5];
+// имя переменной myObj, значения: first: 'First Name', last: 'Last Name'
+var myObj = {first: 'First Name', last: 'Last Name'};
+/*
+ * #2
+ *
+ * Отформатируйте целое число, которое хранится в переменной myNum,
+ * чтобы получить результат с 2 знаками после запятой.
+ * Результат сохраните в переменную decimal2.
+ */
+var decimal2 = myNum;
+console.log(decimal2);
+// decimal2
+var decimal2 = myNum.toFixed(2);
+console.log(decimal2);
+/*
+ * #3
+ *
+ * Создайте переменную i для которой выполните префиксный
+ * и постфиксный инкремент и декремент.
+ * Поэкспериментируйте с результатами, выводя их в консоль.
+ */
+
+// i
+var i = 10;
+console.log (++i); // 11
+console.log (i++);//11
+console.log (i); // 12
+console.log (i--); // 12
+console.log (i);// 11
+console.log (--i); //10
+/*
+ * #4
+ *
+ * Создайте новую переменную myTest и присвойте ей значение 20.
+ * Выполните присваивание с операцией, используя операторы: +=, –=, *=, /=, %=.
+ * Результаты присваиваются в myTest, затем выводятся в консоль.
+ * В вычислениях можно использовать объявленную ранее переменную myNum и/или числа.
+ */
+
+// myTest
+var myTest = 20;
+console.log (myTest);
+// +=
+myTest += 10; 
+console.log (myTest);
+// –=
+myTest -=10;
+console.log (myTest);
+// *=
+myTest *= 10;
+console.log (myTest);
+// /=
+myTest /= 10;
+console.log (myTest);
+// %=
+myTest %= 10;
+console.log (myTest);
+/*
+ * #5
+ *
+ * Используя свойства и методы объекта Math, присвойте переменным и отобразите в консоли
+ */
+
+// константа Pi → myPi
+const myPi = Math.PI;
+console.log(Math.PI);
+// округленное значение числа 89.279 → myRound
+const myRound = Math.round;
+console.log(Math.round(89.279));
+// случайное число между 0..10 → myRandom
+const myRandom = Math.random;
+console.log(Math.random()*10);
+// 3 в 5 степени → myPow
+const myPow = Math.pow;
+console.log(Math.pow(3,5));
+/*
+ * #6
+ *
+ * Создайте объект с именем strObj.
+ * Присвойте ключу str строку текста "Мама мыла раму, рама мыла маму", ключу length установите длину этой строки.
+ */
+var strObj = {};
+console.log(strObj);
+// Мама мыла раму, рама мыла маму
+strObj.str = "Мама мыла раму, рама мыла маму";
+console.log(strObj);
+// strObj
+strObj.length = strObj.str.length;
+console.log(strObj);
+/*
+ * #7
+ *
+ * Проверьте наличие текста 'рама' в поле str объекта strObj (см.п.6),
+ * результат сохраните в переменную isRamaPos и выведите ее в консоль.
+ */
+
+// isRamaPos
+var isRamaPos = strObj.str.indexOf('рама');
+console.log(isRamaPos);
+/*
+ * #8
+ *
+ * Выполните переименование подстроки в строке.
+ * В качестве исходной строки используйте значение поля str объекта strObj (см.п.6), результат сохраните в переменную strReplace и отобразите в консоли.
+ * Исходная строка: 'Мама мыла раму, рама мыла маму'
+ *       Результат: 'Мама моет раму, Рама держит маму'
+ */
+var strReplace = strObj.str.replace('мыла раму, рама мыла','моет раму, Рама держит');
+console.log(strReplace);
+// strReplace
+
+/*
+ * #9
+ *
+ * Преобразуйте любую строку в верхний, затем в нижний регистры, результат отобразите в консоли.
+ */
+var strUpperCase = strObj.str.toUpperCase();
+console.log(strUpperCase);
+var strLowerCase = strObj.str.toLowerCase();
+console.log(strLowerCase);
